fe95efb82694ee52b28c3ea51493289befb3e38c
Peslier Lucien <lucien.headfox@gmail.com>
2016-06-21 15:06:23 +0200

    Fix personnalize feature

--- js/script.js
+++ js/script.js
@@ -15,11 +15,52 @@ $( document ).ready(function() {
   	}
   });
   $(".personalize.main").click(function(){
-    $(".personalize.second").toggle();
-    $(".personalize.third").delay(400).toggle();
+    $(".mini").toggle();
+  });
+  $(".personalize.second").click(function(){
+    $('.before_tabs').removeClass("third-colorize");
+    $('paper-tab').removeClass("third-colorize");
+    $('.before_tabs').addClass("second-colorize");
+    $('paper-tab').addClass("second-colorize");
+    $('.personalize.main').css("color","#FFFFFF");
+    $('.personalize.main').css("background-color","#01BCD4");
+    $('footer paper-button').css("background-color","#01BCD4");
+    $('.parcours').css("background-color","#01BCD4");
+    $('#not_mini').css("background-color","#01BCD4");
+    $(".personalize.fourth").show();
+    $(".personalize.third").css("bottom","-82px");
+    $(".personalize.third").show();
+    $(".personalize.second").hide();
   });
   $(".personalize.third").click(function(){
-    $('.before_tabs').css("background-color", "#3f51b5");
-    $('paper-fab').css("background-color","#FF5252");
+    $('.before_tabs').removeClass("second-colorize");
+    $('paper-tab').removeClass("second-colorize");
+    $('.before_tabs').addClass("third-colorize");
+    $('paper-tab').addClass("third-colorize");
+    $('.personalize.main').css("color","#FFFFFF");
+    $('.personalize.main').css("background-color","#FF4081");
+    $('footer paper-button').css("background-color","#FF4081");
+    $('.parcours').css("background-color","#FF4081");
+    $('#not_mini').css("background-color","#FF4081");
+    $(".personalize.fourth").show();
+    $(".personalize.third").hide();
+    $(".personalize.second").show();
+
+  });
+  $(".personalize.fourth").click(function(){
+    $('.before_tabs').removeClass("second-colorize");
+    $('paper-tab').removeClass("second-colorize");
+    $('.before_tabs').removeClass("third-colorize");
+    $('paper-tab').removeClass("third-colorize");
+    $('.personalize.main').css("color","#FFFFFF");
+    $('.personalize.main').css("background-color","#1DE9B6");
+    $('footer paper-button').css("background-color","#1DE9B6");
+    $('#not_mini').css("background-color","#1DE9B6");
+    $('.parcours').css("background-color","#1DE9B6");
+    $(".personalize.fourth").hide();
+    $(".personalize.third").css("bottom","-135px");
+    $(".personalize.third").show();
+    $(".personalize.second").show();
+
   });
 });
